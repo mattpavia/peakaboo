3c0f728b2e56dba04f4ef2d9d8b3daf21276c27f
jamlamberti <jam.lamberti@yahoo.com>
2014-04-12 16:26:34 -0400

    Added active flag to models

--- models/group.js
+++ models/group.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 var groupSchema = mongoose.Schema({
     id: Number,
     user_1: String,
-    user_2: String
+    user_2: String,
+    active: Boolean
 });
 
 module.exports = mongoose.model('Group', groupSchema);
