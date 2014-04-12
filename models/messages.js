bc9e9b7414ee698e77945f3ba8a8b42522664182
Matthew Pavia <matt.pavia@gmail.com>
2014-04-12 16:02:28 -0400

    Remove extra { }

--- models/messages.js
+++ models/messages.js
@@ -2,14 +2,10 @@
 
 var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
-	{
-		sender : String,
-		receiver : String,
-		data : String,
-		time : Timestamp
-		
-	}
-
+	sender : String,
+	receiver : String,
+	data : String,
+	time : Timestamp
 });
 
 module.exports = mongoose.model('messages', userSchema);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,7 @@
 // user.js
 var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
-	{
-		fbid : String
-	}
-
+	fbid : String
 });
 
 module.exports = mongoose.model('user', userSchema);
