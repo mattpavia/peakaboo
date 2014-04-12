32436d93da720e5706bcbbc634ee7ff8445679a4
jamlamberti <jam.lamberti@yahoo.com>
2014-04-12 15:15:03 -0400

    Reverted database stuff

--- app.js
+++ app.js
@@ -66,11 +66,6 @@ io.on('connection', function(socket) {
 app.get('/', routes.index);
 app.get('/:id', routes.index);
 
-// Added by Jack for database.js test 4/12/14
-db = require('./database');
-db.test;
-
-
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
