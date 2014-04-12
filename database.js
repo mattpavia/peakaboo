6e33b6901cf7c14fa4722e3519216cdc25d778ca
jamlamberti <jamlamberti@users.noreply.github.com>
2014-04-12 14:31:14 -0400

    Fixed typo

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ var db = new db('peekaboo', new Server("mattpavia.com", 27017,
 	{auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
 
 // Establish connection to db
-export.test = function() {
+exports.test = function() {
     db.open(function(err, db) {
         assert.equal(null, err);
         db.authenticate('peekaboo', 'lvhacks2014', function(err, result) {
