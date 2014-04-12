aa5d6515532f416273bec459b02011f0f74b352d
Matthew Pavia <matt.pavia@gmail.com>
2014-04-12 14:36:23 -0400

    testing some things

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
 // lvhacks Peekaboo
 // database.js
 
-var db = require('mongodb').Db,
+// Establish connection to db
+exports.test = function() {
+    var db = require('mongodb').Db,
     Server = require('mongodb').Server,
     assert = require('assert');
 
-var db = new db('peekaboo', new Server("mattpavia.com", 27017,
-	{auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
+    var db = new db('peekaboo', new Server("mattpavia.com", 27017,
+        {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
 
-// Establish connection to db
-exports.test = function() {
     db.open(function(err, db) {
         assert.equal(null, err);
         db.authenticate('peekaboo', 'lvhacks2014', function(err, result) {
@@ -18,5 +18,5 @@ exports.test = function() {
             console.log(err);
         });
     });
+    db.close();
 }
-db.close();
