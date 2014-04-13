44118a7cddc0f70b2c5b4dd9e9656c6efc8858cf
Matthew Pavia <matt.pavia@gmail.com>
2014-04-13 13:32:49 -0400

    fix error

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ module.exports = function(app, server, passport) {
 
             var uid = User.find({'fid' : { $ne : user_1 }}).limit(-1).skip(rand);
             var cnt;
-            Group.count().or([{'user_1' : user_1, 'user_2' : uid.fid}, {'user_1' : uid.fid, 'user_2' : user_1}]).exec(err, cnt);
+            var cnt = Group.count().or([{'user_1' : user_1, 'user_2' : uid.fid}, {'user_1' : uid.fid, 'user_2' : user_1}]);
             var i = 0;
             while ((i<10) && (cnt!=0)) {
                 rand = Math.floor(Math.random()*count);
