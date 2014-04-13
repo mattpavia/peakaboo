4b30f32d870df439d3d70bd5ada930b5ae9d89a7
jamlamberti <jam.lamberti@yahoo.com>
2014-04-12 20:45:23 -0400

    Added clean up function run when page ready

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var color = $('.page').attr('class').split(' ')[1];
     $(".link." + color + "_hover").addClass("active");
+    cleanUp();
 });
 function newChat() {
 	var content = $(".nav").html();
@@ -10,6 +11,27 @@ function newChat() {
 	var cnt = arr.length;
 	var color = colors[cnt%colors.length];
 	var icon = icons[cnt%icons.length];
-	var addition = '<div class="link ' + color + '_hover" data-color="' + color + '"><a href="/' + cnt + '"><i class="fa fa-' + icon + '"></i> <span>Group ' + cnt + '</span></a></div>';
+	var addition = '<div class="link ' + color + '_hover" data-color="' + color + '"><a href="/group/' + cnt + '"><i class="fa fa-' + icon + '"></i> <span>Group ' + cnt + '</span></a></div>';
     $(".nav").append(addition);
+}
+
+function cleanUp() {
+	var colors = ['green', 'teal', 'blue', 'purple', 'red', 'orange', 'yellow'];
+	var icons = ['home', 'comments', 'thumb-tack', 'users', 'user', 'trophy', 'envelope-o'];
+	var content = $(".nav").html();
+	var arr = content.split('div class="link green_hover');
+	var cnt = arr.length;
+	var newHTML = "";
+	for (var i=1; i<cnt; i++) {
+		var color = colors[Math.floor(i/2)%colors.length];
+		var icon = icons[Math.floor(i/2)%icons.length];
+		var j = arr[i].indexOf(">");
+		var datas = arr[i].substring(j);
+		data = datas.split('<i class="fa fa-home">')
+		var data1 = data[0];
+		var data2 = data[1];
+		newHTML += '<div class="link ' + color + '_hover"' + ' data-color="' + color + '"' + data1 + '<i class="fa fa-' + icon + '">' + data2;
+		newHTML = newHTML.substring(0, newHTML.length-1);
+	}
+	$('.nav').html(newHTML);
 }
